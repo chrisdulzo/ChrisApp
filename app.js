baf1415e5bc949122b9123bbce2e781941c7b873
unknown <cdulzo@5C10N32.mi.corp.rockfin.com>
2015-07-28 13:01:36 -0400

    Improved

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+//app.use(express.bodyParser());
+//app.use(app.router);
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('home')
+});
+
+//app.get('/', function(req, res){
+//  res.sendfile("./home.ejs");
+//});
+
+//app.post('/place', function(req, res) {
+//    
+//    res.send('firstname: '+req.body.firstname); 
+//});
+
+app.get('/place', function(req, res){
+    res.send("Welcome " + req.query['firstname'] + req.query['lastname'] + ".");
 });
 
 app.listen(app.get('port'), function() {
